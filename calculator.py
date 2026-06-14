03def1fa4dd3ff92b39f8ed18e0abab382c927f1
mantashakhan9670-arch <mantashakhan9670@gmail.com>
2026-06-14 11:36:29 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,23 @@
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+
+print("1.Addition")
+print("2.Substraction")
+print("3.Multiplication")
+print("4.Division")
+
+choice= input ("Enter choice:")
+
+if choice == '1':
+    print("Result=", num1+num2)
+elif choice == '2':
+    print("Result=", num1-num2)
+elif choice =='3':
+    print("Result=", num1*num2)
+elif choice =='4':
+    print("Result=", num1/num2)
+else:
+    print("Invalid choice")
+
 
