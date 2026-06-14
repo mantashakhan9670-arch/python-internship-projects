a05f880fa69f222f675df45e3ab789fbd9133138
mantashakhan9670-arch <mantashakhan9670@gmail.com>
2026-06-14 11:42:13 +0530

    Update word_counter.py

--- word_counter.py
+++ word_counter.py
@@ -1,1 +1,10 @@
+filename= input("Enter file name:")
+
+try:
+  with open(filename, "r") as file:
+    text= file.read()
+    words= text.split()
+    print("Total words:", len(words))
+except FileNotFoundError
+   print("File not found")
 
